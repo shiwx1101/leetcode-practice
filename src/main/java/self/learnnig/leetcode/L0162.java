008b33ee5e80166812269eb20e16d4b69d1dc394
wenxiang.shi <wenxiang.shi@kone.com>
2026-03-26 15:27:31 +0800

    峰值元素是

---
+++ src/main/java/self/learnnig/leetcode/L0162.java
@@ -0,0 +1,53 @@
+package self.learnnig.leetcode;
+
+/**
+ * 峰值元素是指其值严格大于左右相邻值的元素。
+ * <p>
+ * 给你一个整数数组 nums，找到峰值元素并返回其索引。数组可能包含多个峰值，在这种情况下，返回 任何一个峰值 所在位置即可。
+ * <p>
+ * 你可以假设 nums[-1] = nums[n] = -∞ 。
+ * <p>
+ * 你必须实现时间复杂度为 O(log n) 的算法来解决此问题。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [1,2,3,1]
+ * 输出：2
+ * 解释：3 是峰值元素，你的函数应该返回其索引 2。
+ * 示例 2：
+ * <p>
+ * 输入：nums = [1,2,1,3,5,6,4]
+ * 输出：1 或 5
+ * 解释：你的函数可以返回索引 1，其峰值元素为 2；
+ * 或者返回索引 5， 其峰值元素为 6。
+ *
+ *
+ *
+ */
+public class L0162 {
+
+    public int findPeakElement(int[] nums) {
+
+        int length = nums.length;
+        int left = 0;
+        int right = length - 1;
+        if (length == 1) return 0;
+
+        while (right >= left) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > nums[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+
+        }
+        return left;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new L0162().findPeakElement(new int[]{1, 2, 1}));
+    }
+
+
+}
