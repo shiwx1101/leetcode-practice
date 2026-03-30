d53f03cdf622f255f5ce5ef4bf0b7d8fd93865db
wenxiang.shi <wenxiang.shi@kone.com>
2026-03-30 10:06:45 +0800

    搜索二维矩阵 II

---
+++ src/main/java/self/learnnig/leetcode/L0240.java
@@ -0,0 +1,47 @@
+package self.learnnig.leetcode;
+
+/**
+ *
+ * 编写一个高效的算法来搜索 m x n 矩阵 matrix 中的一个目标值 target 。该矩阵具有以下特性：
+ * <p>
+ * 每行的元素从左到右升序排列。
+ * 每列的元素从上到下升序排列。
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * <p>
+ * 输入：matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 5
+ * 输出：true
+ * 示例 2：
+ * <p>
+ * <p>
+ * 输入：matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]], target = 20
+ * 输出：false
+ * <p>
+ * <p>
+ * 思路:
+ * <p>
+ * 注意左边比x小，右边比x大。
+ *
+ */
+public class L0240 {
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix[0].length;
+        int n = matrix.length;
+        int x = 0;
+        int y = m - 1;
+        while (x < n && y >= 0) {
+            if (matrix[x][y] == target) {
+                return true;
+            } else if (target > matrix[x][y]) {
+                x++;
+            } else {
+                y--;
+            }
+        }
+        return false;
+    }
+
+}
