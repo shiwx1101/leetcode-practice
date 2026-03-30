4e4eee4820e1ab0e0fb05581dff70943c088f406
wenxiang.shi <wenxiang.shi@kone.com>
2026-03-30 16:48:51 +0800

    字符串相加

---
+++ src/main/java/self/learnnig/leetcode/L0415.java
@@ -0,0 +1,65 @@
+package self.learnnig.leetcode;
+
+/**
+ * 给定两个字符串形式的非负整数 num1 和num2 ，计算它们的和并同样以字符串形式返回。
+ * <p>
+ * 你不能使用任何內建的用于处理大整数的库（比如 BigInteger）， 也不能直接将输入的字符串转换为整数形式。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：num1 = "11", num2 = "123"
+ * 输出："134"
+ * 示例 2：
+ * <p>
+ * 输入：num1 = "456", num2 = "77"
+ * 输出："533"
+ * 示例 3：
+ * <p>
+ * 输入：num1 = "0", num2 = "0"
+ * 输出："0"
+ *
+ *  也不能直接将输入的字符串转换为整数形
+ * 注意 数字 char - '0'
+ *
+ */
+public class L0415 {
+
+    public String addStrings(String num1, String num2) {
+
+        int length1 = num1.length();
+        int length2 = num2.length();
+        int a = 0;
+        int forward = 0;
+        StringBuilder stringBuilder = new StringBuilder();
+        while (a < Math.max(length1, length2)) {
+            int tempA = 0;
+            if (length1 - 1 - a >= 0) {
+                tempA =  num1.charAt(length1 - 1 - a) - '0';
+            }
+            int tempB = 0;
+            if (length2 - 1 - a >= 0) {
+                tempB =  num2.charAt(length2 - 1 - a) - '0';
+            }
+            a++;
+            int res = (tempB + tempA + forward) % 10;
+            forward = (tempA + tempB + forward) / 10;
+            stringBuilder.append(res);
+
+        }
+        if(forward ==1){
+            stringBuilder.append(1);
+        }
+        return stringBuilder.reverse().toString();
+
+    }
+
+    public static void main(String[] args) {
+//        char j = '5';
+//        int ff = j;
+//        System.out.println(ff - 48);
+        System.out.println(new L0415().addStrings("1", "9").toString());
+    }
+
+}
