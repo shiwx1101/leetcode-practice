a1656dfe21a82c9102bfe5816fc20f2658e5e2f1
wenxiang.shi <wenxiang.shi@kone.com>
2026-03-31 11:44:36 +0800

    滑动窗口中的最大值 。

---
+++ src/main/java/self/learnnig/leetcode/L0239.java
@@ -0,0 +1,59 @@
+package self.learnnig.leetcode;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ *
+ * 给你一个整数数组 nums，有一个大小为 k 的滑动窗口从数组的最左侧移动到数组的最右侧。你只可以看到在滑动窗口内的 k 个数字。滑动窗口每次只向右移动一位。
+ * <p>
+ * 返回 滑动窗口中的最大值 。
+ * <p>
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [1,3,-1,-3,5,3,6,7], k = 3
+ * 输出：[3,3,5,5,6,7]
+ * 解释：
+ * 滑动窗口的位置                最大值
+ * ---------------               -----
+ * [1  3  -1] -3  5  3  6  7       3
+ * 1 [3  -1  -3] 5  3  6  7       3
+ * 1  3 [-1  -3  5] 3  6  7       5
+ * 1  3  -1 [-3  5  3] 6  7       5
+ * 1  3  -1  -3 [5  3  6] 7       6
+ * 1  3  -1  -3  5 [3  6  7]      7
+ * 示例 2：
+ * <p>
+ * 输入：nums = [1], k = 1
+ * 输出：[1]
+ *
+ * 单调队列，滑动窗口
+ */
+public class L0239 {
+
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        Deque<Integer> deque = new LinkedList<>();
+        int[] result = new int[nums.length - k + 1];
+        for (int i = 0; i < k; i++) {
+            while (!deque.isEmpty() && deque.peekLast() < nums[i]) {
+                deque.removeLast();
+            }
+            deque.addLast(nums[i]);
+        }
+        result[0] = deque.peekFirst();
+        for (int i = k; i < nums.length; i++) {
+            if (deque.peekFirst() == nums[i - k]) {
+                deque.removeFirst();
+            }
+            while (!deque.isEmpty() && deque.peekLast() < nums[i]) {
+                deque.removeLast();
+            }
+            deque.addLast(nums[i]);
+            result[i - k + 1] = deque.peekFirst();
+
+        }
+        return  result;
+    }
+}
